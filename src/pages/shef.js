b5438ac29e64e367570cc543a666140246a4a5b1
Dream-Diver-Co <dreamdiver2017@gmail.com>
2024-02-04 10:11:41 +0600

    Base

--- src/pages/shef.js
+++ src/pages/shef.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Navbar } from '../components';
 import ChefHiringForm from '../components/ChefHiringForm';
 import './Shef.css';
@@ -7,8 +8,8 @@ const Shef = () => {
     <div>
       <Navbar />
       <div className="shef-container">
-        <h1>Hire a Chef for Your Home</h1>
-        <p>Experience the joy of having a chef cook for you at home!</p>
+        <h1 className="shef-heading">Hire a Chef for Your Home</h1>
+        <p className="shef-subheading">Experience the joy of having a chef cook for you at home!</p>
         <ChefHiringForm />
       </div>
     </div>
